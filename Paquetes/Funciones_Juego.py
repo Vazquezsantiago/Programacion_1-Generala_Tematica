5095df87bc121fa94616a6175232c131fff8f0dc
Vazquezsantiago <vazquezsantiago47606@gmail.com>
2025-11-26 17:01:44 -0300

    Cambios funciones
    funcion tirar dados y guardar dados funcionando

--- Paquetes/Funciones_Juego.py
+++ Paquetes/Funciones_Juego.py
@@ -5,16 +5,26 @@ def jugar():
         ronda += 1
         tiradas = 0
         dados_usables = 5 
+        dados_mano=[]
         while tiradas != 3:
-            dados_mano=[]
+            nuevos=[]
+            tiradas += 1
+            print(f"tirada numero |{tiradas}|")
+            print(f"dados en mano |{dados_mano}|")
             lista_dados = tirar_dados(dados_usables)
-            dados_usables , dados_mano = guardar_dados(lista_dados, dados_usables)
+            dados_usables , nuevos  = guardar_dados(lista_dados, dados_usables)
+            dados_mano.extend(nuevos)
+            print(f"dados en mano: {dados_mano}")
+            print("\n=== FIN DE RONDA ===")
+            
     pass
 def tirar_dados(dados_usables):
     lista_dados=[]
     for i in range (dados_usables):
         lista_dados.append(random.randint(1, 6))
+    print("\n=== DADOS ===")
     print(lista_dados)
+    print("\n=============")
     return(lista_dados)
 
 
@@ -26,8 +36,8 @@ def guardar_dados(lista_dados, dados_usables):
     while desea != "n" :
         desea= str(input("desea guardar dados?(s/n): ")).lower()
         if desea == "n":
-            print(lista_dados)
-            break
+            return dados_usables, dados_mano
+        
         print(lista_dados)
         posicion=int(input(f"elija un dado a guardar(1-{dados_usables} por su posicion): "))
         borrado = lista_dados[(posicion-1)]
